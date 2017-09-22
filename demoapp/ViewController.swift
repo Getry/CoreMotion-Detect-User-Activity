5bdd5e118931d1ed24ec4c45a5d9c6b04daf517b
JasonSekhon <jason_2s@hotmail.com>
2017-09-22 00:18:06 -0700

    Initial Commit

---
+++ demoapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoapp
+//
+//  Created by Jason Sekhon on 2017-09-22.
+//  Copyright © 2017 Jason Sekhon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
