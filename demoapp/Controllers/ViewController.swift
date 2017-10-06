8c933546c094452ed2af6cb9ffc9e045d2a7e0ac
JasonSekhon <jason_2s@hotmail.com>
2017-10-06 08:58:43 -0700

    initial

--- demoapp/Controllers/ViewController.swift
+++ demoapp/Controllers/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var musicBtn: UIButton!
+    @IBOutlet weak var motionBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
